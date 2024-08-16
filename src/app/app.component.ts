d7f49761ed4cad775fa5469df2533c8fc229335a
Esley Leal Santana <esley.santana@tely.local>
2024-08-16 20:55:18 -0300

    codigo reorganizado, identacao para melhor legibidade

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { AtivacaoInternetComponent } from './ativacao-internet/ativacao-internet.component';
-import { MudancaEnderecoComponent } from './mudanca-endereco/mudanca-endereco.component';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { DatePipe } from '@angular/common';
+import { AtivacaoInternetComponent } from './ativacao-internet/ativacao-internet.component';
+import { MudancaEnderecoComponent } from './mudanca-endereco/mudanca-endereco.component';
+import { MudancaEnderecoTEquipamentoComponent } from './mudanca-endereco-t-equipamento/mudanca-endereco-t-equipamento.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,AtivacaoInternetComponent, MudancaEnderecoComponent, FormsModule, CommonModule],
+  imports: [
+    RouterOutlet,
+    FormsModule,
+    CommonModule,
+    AtivacaoInternetComponent,
+    MudancaEnderecoComponent,
+    MudancaEnderecoTEquipamentoComponent,
+  ],
   providers: [DatePipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
